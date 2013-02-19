60b4a912c2ecff7949da410c439b959fdab75649
mzmcbride <g@mzmcbride.com>
2013-02-19 01:33:10 -0500

    Fleshed out database scripts.

---
+++ database/create-database.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+# Public domain; MZMcBride; 2013
+
+import ConfigParser
+import os
+import sqlite3
+
+config = ConfigParser.ConfigParser()
+config.read([os.path.expanduser('~/.gerrit-reports.ini')])
+
+database_name = config.get('gerrit-reports', 'database_name')
+
+conn = sqlite3.connect(database_name)
+cursor = conn.cursor()
+cursor.execute('''
+CREATE TABLE changesets (
+  gc_number integer primary key,
+  gc_change_id text,
+  gc_project text,
+  gc_branch text,
+  gc_status text,
+  gc_subject text,
+  gc_created text,
+  gc_updated text,
+  gc_owner text
+);
+''')
+cursor.close()
+conn.commit()
+conn.close()
---
+++ database/update-database.py
@@ -0,0 +1,95 @@
+#! /usr/bin/env python
+# Public domain; MZMcBride; 2013
+
+import ConfigParser
+import json
+import os
+import sqlite3
+import urllib2
+
+config = ConfigParser.ConfigParser()
+config.read([os.path.expanduser('~/.gerrit-reports.ini')])
+
+database_name = config.get('gerrit-reports', 'database_name')
+base_url = config.get('gerrit-reports', 'base_url')
+
+def get_changes(base_url, status, sortkey):
+    changes = []
+    more_changes = False
+    if sortkey:
+        sortkey_param = '+sortkey_before:%s' % sortkey
+    else:
+        sortkey_param = ''
+    params = '?q=status:%s' % status + sortkey_param+'&n=500'
+    opener = urllib2.build_opener()
+    # Set a user agent to avoid an "Authentication required" error
+    opener.addheaders = [('User-Agent', 'gerrit-reports')]
+    url_contents = opener.open(base_url+'changes/' + params).read()
+    # Strip first five bytes to avoid an XSSI protection
+    valid_json = url_contents[5:]
+    loaded_json = json.loads(valid_json)
+    for i in loaded_json:
+        try:
+            i['_more_changes']
+            more_changes = True
+            sortkey = i['_sortkey']
+        except KeyError:
+            pass
+        changes.append(i)
+    return changes, sortkey
+
+def get_cumulative_changes(base_url, status):
+    sortkey = ''
+    sortkeys = []
+    cumulative_changes = []
+    while True:
+        changes, sortkey = get_changes(base_url, status, sortkey)
+        cumulative_changes += changes
+        if sortkey in sortkeys:
+            break
+        if not changes:
+            break
+        else:
+            sortkey = sortkey
+            sortkeys.append(sortkey)
+    return cumulative_changes
+
+total_changes = []
+for status in ['abandoned',
+               'closed',
+               'merged',
+               'open',
+               'reviewed',
+               'submitted']:
+    cumulative_changes = get_cumulative_changes(base_url, status)
+    total_changes += cumulative_changes
+
+conn = sqlite3.connect(database_name)
+cursor = conn.cursor()
+
+for change in total_changes:
+    cursor.execute('''
+    INSERT OR REPLACE INTO changesets
+    (gc_number,
+     gc_change_id,
+     gc_project,
+     gc_branch,
+     gc_status,
+     gc_subject,
+     gc_created,
+     gc_updated,
+     gc_owner)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
+    ''' , (change[u'_number'],
+           change[u'change_id'],
+           change[u'project'],
+           change[u'branch'],
+           change[u'status'],
+           change[u'subject'],
+           change[u'created'],
+           change[u'updated'],
+           change[u'owner'][u'name']))
+
+cursor.close()
+conn.commit()
+conn.close()
