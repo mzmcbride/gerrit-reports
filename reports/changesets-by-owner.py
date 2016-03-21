mzmcbride/gerrit-reports

commit 8e8a1f54a974baf7a7ab602af721d0cf42a95d19

Add "Changesets by owner" report.

reports/changesets-by-owner.py
@@ -1,0 +1,88 @@
+#! /usr/bin/env python
+# Public domain; MZMcBride; 2015
+
+import ConfigParser
+import os
+import sqlite3
+
+import wikitools
+
+config = ConfigParser.ConfigParser()
+config.read([os.path.expanduser('~/.gerrit-reports.ini')])
+
+database_name = config.get('gerrit-reports', 'database_name')
+wiki_api_url = config.get('gerrit-reports', 'wiki_api_url')
+root_page = config.get('gerrit-reports', 'wiki_root_page')
+
+report_title = root_page + 'Changesets by owner'
+report_template = u'''\
+%s
+
+{| class="wikitable sortable plainlinks"
+|- style="white-space:nowrap;"
+! Owner
+! Changesets<br>(total)
+! Changesets<br>(mediawiki/*)
+! Changesets<br>(mediawiki/core)
+%s
+|- class="sortbottom"
+! Total
+! %s
+! %s
+! %s
+|}
+
+%s
+'''
+
+conn = sqlite3.connect(database_name)
+cursor = conn.cursor()
+cursor.execute('''
+SELECT
+  gc_owner,
+  COUNT(*) as total,
+  SUM( gc_project LIKE 'mediawiki/%' ) as open_mediawiki,
+  SUM( gc_project == 'mediawiki/core' ) as open_core
+FROM changesets
+GROUP BY gc_owner
+ORDER BY total DESC;
+''')
+
+output = []
+open_total = 0
+open_mediawiki = 0
+open_core = 0
+for row in cursor.fetchall():
+    table_row = u"""
+|-
+| %s
+| [https://gerrit.wikimedia.org/r/#/q/{{urlencode:owner:"%s"}},n,z %s]
+| [https://gerrit.wikimedia.org/r/#/q/{{urlencode:owner:"%s" project:^mediawiki/.+ status:open}},n,z %s]
+| [https://gerrit.wikimedia.org/r/#/q/{{urlencode:owner:"%s" project:mediawiki/core status:open}},n,z %s]
+""".strip() % (row[0],
+               row[0], row[1],
+               row[0], row[2],
+               row[0], row[3])
+    output.append(table_row)
+    open_total += int(row[1])
+    open_mediawiki += int(row[2])
+    open_core += int(row[3])
+
+wiki = wikitools.Wiki(config.get('gerrit-reports', 'wiki_api_url'))
+wiki.login(config.get('gerrit-reports', 'wiki_username'),
+           config.get('gerrit-reports', 'wiki_password'))
+
+report = wikitools.Page(wiki, report_title)
+report_text = report_template % (config.get('gerrit-reports',
+                                            'wiki_header_template'),
+                                 '\n'.join(output),
+                                 open_total, open_mediawiki, open_core,
+                                 config.get('gerrit-reports',
+                                            'wiki_footer_template'))
+report_text = report_text.encode('utf-8')
+report.edit(report_text,
+            summary=config.get('gerrit-reports', 'wiki_edit_summary'),
+            bot=1)
+
+cursor.close()
+conn.close()

commit f13c4c6460478f46ef42daccfa519e0778b37a02

"Changesets by owner" Gerrit links shouldn't filter to "status:open".
My bad! Apparently a copy-paste-related bug introduced in 8e8a1f54.

reports/changesets-by-owner.py
@@ -57,8 +57,8 @@ for row in cursor.fetchall():
 |-
 | <span id="{{anchorencode:%s}}">%s</span>
 | [https://gerrit.wikimedia.org/r/#/q/{{urlencode:owner:"%s"}},n,z %s]
-| [https://gerrit.wikimedia.org/r/#/q/{{urlencode:owner:"%s" project:^mediawiki/.+ status:open}},n,z %s]
-| [https://gerrit.wikimedia.org/r/#/q/{{urlencode:owner:"%s" project:mediawiki/core status:open}},n,z %s]
+| [https://gerrit.wikimedia.org/r/#/q/{{urlencode:owner:"%s" project:^mediawiki/.+}},n,z %s]
+| [https://gerrit.wikimedia.org/r/#/q/{{urlencode:owner:"%s" project:mediawiki/core}},n,z %s]
 """.strip() % (row[0], row[0],
                row[0], row[1],
                row[0], row[2],